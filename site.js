08e8378f0358018c982f466a44b7a311907706b1
Rick Butterfield <rickbutterfield@Ricks-MacBook-Pro.local>
2018-09-15 10:39:08 +0100

    Change photo

--- site.js
+++ site.js
@@ -1,11 +1,16 @@
 // ServiceWorker is a progressive technology. Ignore unsupported browsers
-if ('serviceWorker' in navigator) {
-  console.log('CLIENT: service worker registration in progress.');
-  navigator.serviceWorker.register('/sw.js').then(function () {
-    console.log('CLIENT: service worker registration complete.');
-  }, function () {
-    console.log('CLIENT: service worker registration failure.');
-  });
-} else {
-  console.log('CLIENT: service worker is not supported.');
+if (window.location.hostname !== "127.0.0.1") {
+  if ('serviceWorker' in navigator) {
+    console.log('CLIENT: service worker registration in progress.');
+    navigator.serviceWorker.register('/sw.js').then(function () {
+      console.log('CLIENT: service worker registration complete.');
+    }, function () {
+      console.log('CLIENT: service worker registration failure.');
+    });
+  } else {
+    console.log('CLIENT: service worker is not supported.');
+  }
+}
+else {
+  console.log('CLIENT: service worker not running locally');
 }
