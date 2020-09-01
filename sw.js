8cd811f356c690fa8c50976443fd761642f6335c
Rick Butterfield <rick.butterfield@shoutdigital.com>
2020-09-01 12:47:33 +0100

    Change SW to not cache documents

--- sw.js
+++ sw.js
@@ -31,6 +31,14 @@ self.addEventListener("install", function (event) {
 self.addEventListener("fetch", function (event) {
   let requestUrl = event.request.url;
   let isGetRequest = event.request.method === 'GET';
+
+  let destination = event.request.destination;
+  let isFont = destination === 'font';
+  let isDocument = destination === 'document';
+  let isImage = destination === 'image' || destination === '' && requestUrl.endsWith('.svg');
+  let isScript = destination === 'script';
+  let isStyle = destination === 'style';
+
   let isWhitelistedUrl =
     requestUrl.startsWith('http://127.0.0.1') ||
     requestUrl.startsWith('http://localhost') ||
@@ -42,23 +50,25 @@ self.addEventListener("fetch", function (event) {
     return;
   }
 
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      function fetchAndCache() {
-        return fetch(event.request).then(response => {
-          let cacheCopy = response.clone();
+  if (isFont || isImage || isScript || isStyle) {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        function fetchAndCache() {
+          return fetch(event.request).then(response => {
+            let cacheCopy = response.clone();
 
-          caches.open(version).then(cache => cache.put(event.request, cacheCopy));
-          return response;
-        }).catch(error => {
-          return caches.match(offlinePage);
-        });
-      }
+            caches.open(version).then(cache => cache.put(event.request, cacheCopy));
+            return response;
+          }).catch(error => {
+            return caches.match(offlinePage);
+          });
+        }
 
-      if (!response) { return fetchAndCache(); }
-      return response;
-    })
-  );
+        if (!response) { return fetchAndCache(); }
+        return response;
+      })
+    );
+  }
 });
 
 
